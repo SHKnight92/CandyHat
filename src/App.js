011f38fc459dfd73b2955228c43521f49ae96c07
Saddam hossain knight <knight.ru.bd.com>
2021-04-30 21:37:15 +0600

    update

---
+++ src/App.js
@@ -0,0 +1,63 @@
+import { createContext, useState } from "react";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import "./App.css";
+import Checkout from "./components/Checkout/Checkout";
+import Dashboard from "./components/Dashboard/Dashboard";
+import Deals from "./components/Deals/Deals";
+import Header from "./components/Header/Header";
+import Login from "./components/Login/Login";
+import NotFound from "./components/NotFound/NotFound";
+import Orders from "./components/Orders/Orders";
+import PrivateRoute from "./components/PrivateRoute/PrivateRoute";
+import Products from "./components/Products/Products";
+import Search from "./components/Search/Search";
+
+export const UserContext = createContext();
+
+function App() {
+  const [loggedUserData, setLoggedUserData] = useState({
+    isLoggedIn: false,
+    userName: "",
+    email: "",
+    image: "",
+    error: "",
+    handleSignOut: null,
+  });
+
+  return (
+    <UserContext.Provider value={[loggedUserData, setLoggedUserData]}>
+      <Router>
+        <Switch>
+          <Route exact path="/">
+            <Header></Header>
+            <Search></Search>
+            <Products></Products>
+          </Route>
+          <PrivateRoute path="/admin">
+            <Dashboard></Dashboard>
+          </PrivateRoute>
+          <PrivateRoute path="/checkout/:id">
+            <Header></Header>
+            <Checkout></Checkout>
+          </PrivateRoute>
+          <PrivateRoute path="/orders">
+            <Header></Header>
+            <Orders></Orders>
+          </PrivateRoute>
+          <PrivateRoute path="/deals">
+            <Header></Header>
+            <Deals></Deals>
+          </PrivateRoute>
+          <Route path="/login">
+            <Login></Login>
+          </Route>
+          <Route path="*">
+            <NotFound></NotFound>
+          </Route>
+        </Switch>
+      </Router>
+    </UserContext.Provider>
+  );
+}
+
+export default App;
---
+++ src/components/Login/firebase.config.js
@@ -0,0 +1,9 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyClLtxIvaLmArcC0YO9ONavtNRjvBUrbfU",
+  authDomain: "chocolatehat01.firebaseapp.com",
+  projectId: "chocolatehat01",
+  storageBucket: "chocolatehat01.appspot.com",
+  messagingSenderId: "868616492646",
+  appId: "1:868616492646:web:e10cb972e70e12b83f7fa7",
+};
+export default firebaseConfig;
